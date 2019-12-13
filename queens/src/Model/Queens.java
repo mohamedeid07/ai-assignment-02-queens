cbba5126beb9106a3bc47bb8b78286d58f9966db
Mohamed <mohamedeid798@gmail.com>
2019-12-13 17:54:26 +0200

    queens main

--- queens/src/Model/Queens.java
+++ queens/src/Model/Queens.java
@@ -6,6 +6,18 @@ public class Queens {
 	public static void main(String[] args) {
 		ReadInput r = new ReadInput();
 		char[][] input = r.read();
+		System.out.println("CSP:");
+		CSP csp = new CSP();
+		csp.solve(input);
+		System.out.println("Hill Climbing:");
+		HillClimbing hc = new HillClimbing();
+		hc.solve(input);
+		System.out.println("K-Beam:");
+		KBeam kb = new KBeam();
+		kb.solve(input);
+		System.out.println("Genetic:");
+		Genetic genetic = new Genetic();
+		genetic.solve(input);
 		
 	}
 
